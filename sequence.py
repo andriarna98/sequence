128a43191d3939fa5771bc5406359e4473388495
Andri Árnason <andri@Andris-MacBook-Pro.local>
2018-09-04 16:15:10 +0000

    Algorithm for sequence

---
+++ sequence.py
@@ -0,0 +1,15 @@
+n = int(input("Enter the length of the sequence: ")) # Do not change this line
+a = 1
+print (a)
+b = 2
+print(b)
+c = 3
+print(c)
+for tala in range (n):
+    d = a+b+c
+    a = b
+    b = c
+    c = d
+    print(d) 
+
+    
